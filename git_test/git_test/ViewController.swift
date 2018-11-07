3b9adf59f780524d708685a4966e159f5aa09013
張善傑 <s0935683552@gmail.com>
2018-11-07 10:07:13 +0800

    init

---
+++ git_test/git_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_test
+//
+//  Created by 張善傑 on 2018/11/7.
+//  Copyright © 2018年 張善傑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
